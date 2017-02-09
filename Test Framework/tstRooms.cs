f8f8873374ec7a1b366b7a037c58624a3dfbc3fe
p15206117 <p15206117@GWHXX682WIN007W>
2017-02-09 17:14:26 +0000

    creation of clsRooms + test class

---
+++ Test Framework/tstRooms.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Class_Library;
+
+namespace Test_Framework
+{
+    /// <summary>
+    /// Summary description for tstRooms
+    /// </summary>
+    [TestClass]
+    public class tstRooms
+    {
+        public tstRooms()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsRooms ARoom = new clsRooms(); //create an instance of our class clsRooms
+            Assert.IsNotNull(ARoom); //check to see that the class is not null
+        }
+    }
+}
